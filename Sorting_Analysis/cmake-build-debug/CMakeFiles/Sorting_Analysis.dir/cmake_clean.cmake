9fbb000652574fe51ef238f28fdc1923195fabc1
EliasLMann <70070619+EliasLMann@users.noreply.github.com>
2021-03-19 19:43:05 -0500

    Starting Point

---
+++ Sorting_Analysis/cmake-build-debug/CMakeFiles/Sorting_Analysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorting_Analysis.dir/main.cpp.o"
+  "Sorting_Analysis"
+  "Sorting_Analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sorting_Analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sorting_Analysis/cmake-build-debug/CMakeFiles/Sorting_Analysis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sorting_Analysis.
+# This may be replaced when dependencies are built.
