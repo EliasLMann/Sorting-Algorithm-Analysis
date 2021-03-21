691aebbbe96f48d67111f7137d050c208a508137
EliasLMann <70070619+EliasLMann@users.noreply.github.com>
2021-03-21 02:13:47 -0500

    All algorithms function
    Handoff Implementation missing

--- Sorting_Analysis/main.cpp
+++ Sorting_Analysis/main.cpp
@@ -6,12 +6,35 @@
 #include "MergeSort.h"
 #include "RadixSort.h"
 
+//global const for the direct path to the Datasets
 const string DATASET_PATH = "/Users/eliasmann/Desktop/Algorithms/21s-pa02-EliasLMann/Sorting_Analysis/Datasets";
 
+
 int main() {
-    RadixSort test;
-    test.Load(DATASET_PATH);
-    test.Execute();
-    test.Stats();
+    //a vector of all sorting algorithms
+
+    //creating pointers to each sorting object allocated in the heap
+    SelectionSort* selection = new SelectionSort;
+    InsertionSort* insertion = new InsertionSort;
+    HeapSort* heap = new HeapSort;
+    QuickSort* quick = new QuickSort;
+    RadixSort* radix = new RadixSort;
+    MergeSort* merge = new MergeSort;
+
+    //adding all sorting algorithm pointers to an array
+    AlgorithmStrategy* sortingAlgos[6];
+    sortingAlgos[0] = selection;
+    sortingAlgos[1] = insertion;
+    sortingAlgos[2] = heap;
+    sortingAlgos[3] = quick;
+    sortingAlgos[4] = radix;
+    sortingAlgos[5] = merge;
+
+    //executes all algorithms
+    for(int i = 0; i < 6; i++){
+        sortingAlgos[i]->Load(DATASET_PATH);
+        sortingAlgos[i]->Execute();
+        sortingAlgos[i]->Stats();
+    }
     return 0;
 }
